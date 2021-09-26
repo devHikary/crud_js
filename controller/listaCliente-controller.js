3501e7ee92177f86b8b944eadaf07b4a210e8957
devHikary <wrkkarina@gmail.com>
2021-09-26 11:33:35 -0300

    removendo o cliente

--- controller/listaCliente-controller.js
+++ controller/listaCliente-controller.js
@@ -1,6 +1,6 @@
 import { clienteService } from '../service/cliente-service.js';
 
-const criaNovaLinha = (nome, email) => {
+const criaNovaLinha = (nome, email, id) => {
   const linhaNovoCliente = document.createElement('tr');
   const conteudo =
     `
@@ -8,19 +8,34 @@ const criaNovaLinha = (nome, email) => {
     <td>${email}</td>
     <td>
       <ul class="tabela__botoes-controle">
-          <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-          <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
       </ul>
     </td>
   `
   linhaNovoCliente.innerHTML = conteudo
+  linhaNovoCliente.dataset.id = id
+
   return linhaNovoCliente
 }
 
 const tabela = document.querySelector('[data-tabela]');
 
+tabela.addEventListener('click', (evento) => {
+  let ehBotaoDeletar = evento.target.className === 'botao-simples botao-simples--excluir'
+
+  if(ehBotaoDeletar){
+    const linhaCliente = evento.target.closest('[data-id]')
+    let id = linhaCliente.dataset.id
+    clienteService.removeCliente(id)
+    .then(() => {
+      linhaCliente.remove()
+    })
+  }
+})
+
 clienteService.listaClientes().then(data => {
   data.forEach(elemento => {
-    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email, elemento.id))
   })
 })
